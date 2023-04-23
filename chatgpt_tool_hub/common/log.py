e505c091f613829336531caf13317669536cdf39
goldfishh <q8584@qq.com>
2023-04-23 13:52:05 +0800

    fix: terminal 中文化 && 调试

--- chatgpt_tool_hub/common/log.py
+++ chatgpt_tool_hub/common/log.py
@@ -7,11 +7,12 @@ from chatgpt_tool_hub.common.constants import LOGGING_LEVEL, LOGGING_FMT, LOGGIN
 def _get_logger():
     logger = logging.getLogger("tool")
 
-    logger.setLevel(LOGGING_LEVEL)
     ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(LOGGING_LEVEL)
     ch.setFormatter(logging.Formatter(LOGGING_FMT, datefmt=LOGGING_DATEFMT))
 
     fh = logging.FileHandler(f'{os.getcwd()}/tool.log', encoding='utf-8')
+    fh.setLevel(logging.DEBUG)
     fh.setFormatter(logging.Formatter(LOGGING_FMT, datefmt=LOGGING_DATEFMT))
 
     logger.addHandler(ch)
--- chatgpt_tool_hub/tools/__init__.py
+++ chatgpt_tool_hub/tools/__init__.py
@@ -1,10 +1,25 @@
+import os
+
 from chatgpt_tool_hub.common.log import LOG
 
 
+def get_packages(path):
+    """获取指定路径下的所有包名"""
+    packages = []
+    for name in os.listdir(path):
+        full_path = os.path.join(path, name)
+        if os.path.isdir(full_path) and os.path.exists(os.path.join(full_path, "__init__.py")):
+            packages.append(name)
+    return packages
+
+
 def dynamic_tool_loader():
-    # dev tool: debug   visual_dl  searxng_search
-    all_tool_package_list = ["bing_search", "google_search", "meteo", "news", "arxiv_search",
-                             "system", "web_requests", "wikipedia", "wolfram_alpha"]
+    try:
+        all_tool_package_list = get_packages(f"{os.path.dirname(os.path.abspath(__file__))}")
+    except Exception as e:
+        LOG.debug(f"get_packages error: {repr(e)}")
+        all_tool_package_list = ["meteo", "system", "web_requests", "wikipedia"]
+        LOG.debug(f"Detected main tool package: {repr(all_tool_package_list)}")
 
     for package_name in all_tool_package_list:
         try:
@@ -25,5 +40,6 @@ __all__ = [
     "PythonREPLTool",
     "TerminalTool",
     "BrowserTool",
+    "get_packages",
     "dynamic_tool_loader"
 ]
